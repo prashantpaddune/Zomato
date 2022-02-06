b85e91d3082af86c023a0d8b479b906fc3df766c
prashantp01 <prashant.paddune@cogoport.com>
2022-02-06 18:50:00 +0530

    update changes
    Signed-off-by: prashantp01 <prashant.paddune@cogoport.com>

---
+++ client/pages/home/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import {Container} from "./styles";
+
+export default function Home() {
+  return (
+    <Container>
+      <Head>
+        <title>Zomato</title>
+        <meta name="description" content="Zomato clone" />
+      </Head>
+        <h1>WIP</h1>
+    </Container>
+  )
+}
---
+++ client/pages/home/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+  font-family: 'Nunito', sans-serif;
+`;
